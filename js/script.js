fc5a7bd55d1f45ae72205c33df85952291fc172f
Maryna Gaponenko <maryna.a.gaponenko@gmail.com>
2016-03-29 22:31:22 +0300

    New

---
+++ js/script.js
@@ -0,0 +1,33 @@
+﻿'use strict'
+$(function(){
+    $('.tabs__content .tabs__item').not(':first').hide();
+	
+	$('.wrapper__menu .tabs__title').click(function(e){
+	e.preventDefault();
+	
+	$('.wrapper__menu .tabs__title').removeClass('selected').eq($(this).index()).addClass('selected');
+	$('.tabs__content .tabs__item').hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass('selected');
+	
+	
+
+	
+	$('form div.tooltip_box').hover(
+		function(){
+		$(this).find('span').stop().animate({opacity: 1});
+		},
+		function(){
+		$(this).find('span').stop().animate({opacity: 0});
+		}),
+	
+	$('div input.button').click(function() {
+		$('form span').stop().animate({opacity: 1});
+	});
+});
+ 
+ 
+
+ 
+ 
+
+
